75fe8e02be24580f60faec2d3a5a8b659e11012c
Norbert Schneider <Norbert-Schneider@gmx.de>
2012-07-16 00:16:36 +0200

    Pushed functions in separate modules to not pollute the global namespace.
    All functions in background.js are now in the module
    "JustKeys". Currently only the dispatch function is worth "publishing"
    and even that is questionable. In the frontend (contentscript.js) all
    functions are in an anonymous functions.

--- background.js
+++ background.js
@@ -1,28 +1,43 @@
-/*
- * Message Handling Setup
- */
-chrome.extension.onRequest.addListener(function(request, sender, response) {
-    switch(request.action) {
-    case "keybindings":
-        replyKeybindings(request, sender, response);
-        break;
-    case "tabs":
-        replyTabs(request, sender, response);
-        break;
-    }
-});
+(function() {
+  
+  // TODO: Load keybindings from preferences
+  var KEYBINDINGS = {showTabs: "ctrl+b"};
+  
+  // Reply Functions
+  function _replyKeybindings(request, sender, response) {
+    response(KEYBINDINGS);
+  };
 
-
-/**
- * Reply Functions
- */
-function replyKeybindings(request, sender, response) {
-    // TODO: Load keybindings from preferences
-    response({showTabs: "ctrl+b"});
-};
-
-function replyTabs(request, sender, response) {
+  function _replyTabs(request, sender, response) {
     //chrome query directly sends the response
     chrome.tabs.query({currentWindow: true}, response);
-};
-
+  };
+  
+  var justKeys = {
+    
+    /**
+     * Dispatches the received message from the `JustKeys` frontend.
+     * The messages should have the following type:
+     * {action: "Name of the action" [, <extra option properties if needed>]}
+     */
+    dispatch: function(request, sender, response) {
+      switch(request.action) {
+      case "keybindings":
+        _replyKeybindings(request, sender, response);
+        break;
+      case "tabs":
+        _replyTabs(request, sender, response);
+        break;
+      }
+    }
+  };
+  
+  // Expose `JustKeys` to the global object
+  window.JustKeys = justKeys;
+   
+})();
+ 
+ /**
+  * Message Handling Setup
+  */
+ chrome.extension.onRequest.addListener(JustKeys.dispatch);
--- contentscript.js
+++ contentscript.js
@@ -1,15 +1,16 @@
-/**
- * Setup Functions
- */
-chrome.extension.sendRequest({action:"keybindings"}, function(keybindings) {
-    Mousetrap.bind(keybindings["showTabs"], showTabs);
-});
-
-/*
- * Key Binding Functions
- */
-function showTabs() {
+(function() {
+  
+  function _showTabs() {
     chrome.extension.sendRequest({action:"tabs"}, function(response) {
-        console.log("Tabs", response);
-    } );
-}
+      console.log("Tabs", response);
+    });
+  }
+  
+  /**
+   * Setup. Hook all keybindings into the current site.
+   */
+  chrome.extension.sendRequest({action:"keybindings"}, function(keybindings) {
+    Mousetrap.bind(keybindings["showTabs"], _showTabs);
+  });
+  
+}) ();
