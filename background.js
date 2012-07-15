f0a246a82f499afca71ac9b9fdb6a236b675dc85
Norbert Schneider <Norbert-Schneider@gmx.de>
2012-07-15 23:32:00 +0200

    Cleanup.
    Changed some variable names.

--- background.js
+++ background.js
@@ -1,13 +1,13 @@
 /*
  * Message Handling Setup
  */
-chrome.extension.onRequest.addListener(function (request, sender, senderResponse) {
+chrome.extension.onRequest.addListener(function(request, sender, response) {
     switch(request.action) {
     case "keybindings":
-        replyKeybindings(request, sender, senderResponse);
+        replyKeybindings(request, sender, response);
         break;
     case "tabs":
-        replyTabs(request, sender, senderResponse);
+        replyTabs(request, sender, response);
         break;
     }
 });
@@ -16,12 +16,13 @@ chrome.extension.onRequest.addListener(function (request, sender, senderResponse
 /**
  * Reply Functions
  */
-function replyKeybindings(request, sender, sendResponse) {
+function replyKeybindings(request, sender, response) {
     // TODO: Load keybindings from preferences
-    sendResponse({"showTabs": "ctrl+b"});
+    response({showTabs: "ctrl+b"});
 };
 
-function replyTabs(request, sender, sendResponse) {
-    chrome.tabs.query({"currentWindow":true}, sendResponse);
+function replyTabs(request, sender, response) {
+    //chrome query directly sends the response
+    chrome.tabs.query({currentWindow: true}, response);
 };
 
