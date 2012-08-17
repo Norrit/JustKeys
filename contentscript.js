13c9f94eead4d9991327497c47862f396e667474
unknown <Norrit@.(none)>
2012-08-17 22:15:57 +0200

    Filtered elements will now automatically be marked appropriately.

--- contentscript.js
+++ contentscript.js
@@ -10,75 +10,102 @@
     var text = "";
 
     var selection = {
-        highlightableElements: function () {
-            var shouldHighlight = function (element) {
-                return $h.hasLink(element) && $h.elementInViewport(element) && $h.isVisible(element);
-            };
-            var elements = document.getElementsByTagName("a");
-            var highlightable = {};
-            var number = 1;
-            for (var i = 0; i < elements.length; i++) {
-                if (shouldHighlight(elements[i])) {
-                    highlightable[number++] = elements[i];
+        highlightedElements: function (elements) {
+            var selected = this.filteredElements(elements);
+            var highlighted = {};
+            for (var i in elements) {
+                if (!selected[i]) {
+                    highlighted[i] = elements[i];
                 }
             }
-            return highlightable;
+            return highlighted;
         },
 
-        filteredElements: function () {
+        filteredElements: function (elements) {
             var selection = {};
             for (var i in elements) {
-                if (i.toString().indexOf(text) === 0) {
+                if (text != "" && i.toString().indexOf(text) === 0) {
                     selection[i] = elements[i];
                 }
             }
             return selection;
         },
 
-        countFilteredElements: function () {
-            return Object.keys(this.filteredElements()).length;
+        countFilteredElements: function (elements) {
+            return Object.keys(this.filteredElements(elements)).length;
         }
     };
 
-    var highlights = {
-        reset: function () {
-            elements = null;
-            $h.removeClassFromAllElements(justKeysHighlightClass);
-            $h.removeClassFromAllElements(justKeysFilteredClass);
-            $h.removeElementsWithClass(justKeysHighlightNumberClass);
-            $h.removeElementsWithClass(justKeysFilteredNumberClass);
-        },
 
-        highlightElement: function (element, text) {
+
+    var highlights = (function () {
+        function highlightElement(element, text, highlightClass, numberClass) {
             var label = document.createElement("span");
             label.innerText = text;
-            $h.addClass(label, justKeysHighlightNumberClass);
-            $h.addClass(element, justKeysHighlightClass);
+            $h.addClass(label, numberClass);
+            $h.addClass(element, highlightClass);
             $h.insertAsFirst(element, label);
-        },
+        }
+        return {
+            reset: function () {
+                $h.removeClassFromAllElements(justKeysHighlightClass);
+                $h.removeClassFromAllElements(justKeysFilteredClass);
+                $h.removeElementsWithClass(justKeysHighlightNumberClass);
+                $h.removeElementsWithClass(justKeysFilteredNumberClass);
+            },
 
-        highlightElements: function () {
-            for (var i in elements) {
-                this.highlightElement(elements[i], i);
+            highlightElements: function (elements) {
+                this.reset();
+                var highlighted = selection.highlightedElements(elements);
+                for (var i in highlighted) {
+                    highlightElement(highlighted[i], i, justKeysHighlightClass, justKeysHighlightNumberClass);
+                }
+                var filtered = selection.filteredElements(elements);
+                for (var n in filtered) {
+                    highlightElement(filtered[n], n, justKeysFilteredClass, justKeysFilteredNumberClass);
+                }
+            }
+        };
+    })();
+
+    function highlightableElements() {
+        var shouldHighlight = function (element) {
+            return $h.hasLink(element) && $h.elementInViewport(element) && $h.isVisible(element);
+        };
+        var elements = document.getElementsByTagName("a");
+        var highlightable = {};
+        var number = 1;
+        for (var i = 0; i < elements.length; i++) {
+            if (shouldHighlight(elements[i])) {
+                highlightable[number++] = elements[i];
             }
         }
-    };
+        return highlightable;
+    }
 
     function bindSelectionKeys() {
-        var bindSelectionNumberKey = function(index) {
+        var bindSelectionNumberKey = function (index) {
             bindKeys(index, function () {
                 text += index;
+                highlights.highlightElements(elements);
             });
         };
         bindKeys("esc", function () {
-            highlights.reset();
+            reset();
         });
-        bindKeys("backspace", function () {
+        bindKeys("d", function () {
             text = text.substring(0, text.length - 1);
+            highlights.highlightElements(elements);
         });
         for (var i = 0; i < 10; i++) {
             bindSelectionNumberKey(i.toString());
         }
+    }
+
+    function reset() {
+        elements = null;
+        text = "";
+        highlights.reset();
     }
 
     //
@@ -86,9 +113,9 @@
     //
 
     function initFollowLink() {
-        highlights.reset();
-        elements = selection.highlightableElements();
-        highlights.highlightElements();
+        reset();
+        elements = highlightableElements();
+        highlights.highlightElements(elements);
         bindSelectionKeys();
     }
 
